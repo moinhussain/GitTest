96164c2a43dec356dc98a3e8f2e798876bc23f42
Moin Hussain <moinhussain.md@gmail.com>
2017-10-28 21:54:30 -0400

    add function implemented

--- index.php
+++ index.php
@@ -12,8 +12,8 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        function add(){
-            
+        function add($a,$b){
+            return $a + $b;
         }
         function subtract($a, $b){
            return $a - $b; 
